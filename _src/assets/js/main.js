c82c7ae5c00b3f63ecb728948bd0b2ef22c6d980
silvia <silvia.alonso.pozas@gmail.com>
2020-03-27 16:38:04 +0100

    Gets data from API

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,21 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
+
+let users = [];
+
+const getApiData = () => {
+  fetch("https://randomuser.me/api/?results=10")
+    .then(response => response.json())
+    .then(data => {
+      for (const user of data) {
+        name : name,
+        city : location.city,
+        photo : picture.medium,
+        username : login.username
+      }
+      console.log(data);
+    });
+};
+
+getApiData();
