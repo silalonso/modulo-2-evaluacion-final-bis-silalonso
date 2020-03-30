3560e16f4af88efe2ec4f475e362e048b1f5f1e3
silvia <silvia.alonso.pozas@gmail.com>
2020-03-30 22:21:54 +0200

    change background color

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -30,9 +30,16 @@ console.log(users);
 const usersElement = document.querySelector(".js-users");
 
 const getUsersHtmlCode = (user, index) => {
+  console.log(user.isFriend);
   let htmlCode = "";
   htmlCode += `<section class="userSpace" >`;
-  htmlCode += `<ul class="profile" id="${index}">`;
+
+  if (user.isFriend === true) {
+    htmlCode += `<ul class="profile isFriend" id="${index}">`;
+  } else {
+    htmlCode += `<ul class="profile" id="${index}">`;
+  }
+
   htmlCode += `<li class="name">${user.name.first}</li>`;
   htmlCode += `<li class="city">${user.city}</li>`;
   htmlCode += `<li class="image"><img src="${user.picture}" alt="${user.name.first}"></li>`;
@@ -55,36 +62,53 @@ const paintUsers = () => {
 };
 function listenUsers() {
   const userToClick = document.querySelectorAll(".profile");
-  console.log(userToClick);
+  // console.log(userToClick);
   for (const user of userToClick) {
-    console.log(user);
+    // console.log(user);
     user.addEventListener("click", handleUserToClick);
   }
-
-  function handleUserToClick(ev) {
-    // console.log(user);
-    const clickedUser = ev.target.id;
-    const encontrar = users.findIndex(function findClickedUser(user) {
-      // console.log();
-      return user.id === clickedUser;
-    });
-    console.log(clickedUser);
-
-    // console.log(ev.target.id);
-    for (const user of users) {
-      if (clickedUser !== null) {
-        user.isFriend = true;
-      }
-
-      console.log(user);
-    }
-  }
 }
-getApiData();
 
+const handleUserToClick = ev => {
+  //obtener el id del user clickado
+  const clickedUser = parseInt(ev.target.id);
+  console.log(clickedUser);
+  //buscar el user clickado
+  // console.log(users[clickedUser]);
+  let encontrado = users[clickedUser];
+
+  if (encontrado.isFriend === true) {
+    encontrado.isFriend = false;
+  } else {
+    encontrado.isFriend = true;
+  }
+
+  paintUsers();
+};
+
+getApiData();
 // tengo que consolear el index del elemento clicado (en el console.log de la línea 63) y luego lo de añadirle isFriend:true
 
 // user.isFriend = true;
 // // Object.defineProperty(user, "isFriend", { value: true });
 
-// voy a hacer una prueba a ver si se guarda tambien en Master
+// a partir de la 64:
+
+// function handleUserToClick(ev) {
+//   // console.log(user);
+//   const clickedUser = ev.target.id;
+//   const encontrar = users.findIndex(function findClickedUser(user) {
+//     // console.log();
+//     return user.id === clickedUser;
+//   });
+//   console.log(clickedUser);
+
+//   // console.log(ev.target.id);
+//   for (const user of users) {
+//     if (clickedUser !== null) {
+//       user.isFriend = true;
+//     }
+
+//     console.log(user);
+//   }
+// }
