1bdf1fab54d5523eda60c98d202927626793f7af
silvia <silvia.alonso.pozas@gmail.com>
2020-03-31 16:48:07 +0200

    clean code

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-console.log(">> Ready :)");
-
 let users = [];
 
 const getApiData = () => {
@@ -9,9 +7,6 @@ const getApiData = () => {
     .then(response => response.json())
     .then(function(data) {
       const datos = data.results;
-
-      // console.log(datos);
-
       for (const dato of datos) {
         let user = {
           name: dato.name.first,
@@ -19,7 +14,6 @@ const getApiData = () => {
           picture: dato.picture.medium,
           username: dato.login.username
         };
-        // console.log(user);
         users.push(user);
       }
       paintUsers();
@@ -40,9 +34,9 @@ const getUsersHtmlCode = (user, index) => {
     htmlCode += `<ul class="profile" id="${index}">`;
   }
 
-  htmlCode += `<li class="name">${user.name.first}</li>`;
+  htmlCode += `<li class="name">${user.name}</li>`;
   htmlCode += `<li class="city">${user.city}</li>`;
-  htmlCode += `<li class="image"><img src="${user.picture}" alt="${user.name.first}"></li>`;
+  htmlCode += `<li class="image"><img src="${user.picture}" alt="${user.name}"></li>`;
   htmlCode += `<li class= "username">${user.username}</li>`;
   htmlCode += `</ul>`;
   htmlCode += `</section>`;
@@ -53,7 +47,6 @@ const paintUsers = () => {
   let usersCode = "";
   for (let index = 0; index < users.length; index++) {
     const user = users[index];
-
     usersCode += getUsersHtmlCode(user, index);
   }
 
@@ -62,53 +55,23 @@ const paintUsers = () => {
 };
 function listenUsers() {
   const userToClick = document.querySelectorAll(".profile");
-  // console.log(userToClick);
   for (const user of userToClick) {
-    // console.log(user);
     user.addEventListener("click", handleUserToClick);
   }
 }
 
 const handleUserToClick = ev => {
-  //obtener el id del user clickado
   const clickedUser = parseInt(ev.target.id);
-  console.log(clickedUser);
-  //buscar el user clickado
-  // console.log(users[clickedUser]);
-  let encontrado = users[clickedUser];
 
-  if (encontrado.isFriend === true) {
-    encontrado.isFriend = false;
+  let foundUser = users[clickedUser];
+
+  if (foundUser.isFriend === true) {
+    foundUser.isFriend = false;
   } else {
-    encontrado.isFriend = true;
+    foundUser.isFriend = true;
   }
 
   paintUsers();
 };
 
 getApiData();
-// tengo que consolear el index del elemento clicado (en el console.log de la línea 63) y luego lo de añadirle isFriend:true
-
-// user.isFriend = true;
-// // Object.defineProperty(user, "isFriend", { value: true });
-
-// a partir de la 64:
-
-// function handleUserToClick(ev) {
-//   // console.log(user);
-//   const clickedUser = ev.target.id;
-//   const encontrar = users.findIndex(function findClickedUser(user) {
-//     // console.log();
-//     return user.id === clickedUser;
-//   });
-//   console.log(clickedUser);
-
-//   // console.log(ev.target.id);
-//   for (const user of users) {
-//     if (clickedUser !== null) {
-//       user.isFriend = true;
-//     }
-
-//     console.log(user);
-//   }
-// }
